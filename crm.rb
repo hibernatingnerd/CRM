3d2956c9cceb6167ac7684c9d84152909dc1103a
hibernatingnerd <jeffery.mark.campbell@gmail.com>
2018-06-11 14:04:06 -0400

    add edit function using a put request

--- crm.rb
+++ crm.rb
@@ -39,7 +39,7 @@ end
 # end
 get '/' do
   @contacts = Contact.all
-  @num_contacts = @contacts.count
+  @totalnumberofcontacts = @contacts.count
   erb :contacts
 end
 
@@ -61,6 +61,30 @@ post '/contacts' do
   redirect to('/contacts')
 end
 
+get '/contacts/:id/edit' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
 
 after do
   ActiveRecord::Base.connection.close
