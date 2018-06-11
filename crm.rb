acd295565042b50d895e5facb4cce53ddd907afa
hibernatingnerd <jeffery.mark.campbell@gmail.com>
2018-06-11 13:28:53 -0400

    add input page. update crm.rb for POST requests for form.

--- crm.rb
+++ crm.rb
@@ -10,10 +10,10 @@ get '/about' do
   erb :about
 end
 
-get '/contacts' do
-  @contacts = Contact.all
-  erb :contacts
-end
+# get '/contacts' do
+#   @contacts = Contact.all
+#   erb :contacts
+# end
 
 get '/contacts/:id' do
   @contact = Contact.find_by(id: params[:id].to_i)
@@ -24,8 +24,41 @@ get '/contacts/:id' do
   end
 end
 
-get '/new' do
+# post '/new' do
+#   user_first_name =  params[:firstname]
+#   user_last_name = params[:lastname]
+#   user_email = params[:email]
+#   user_note = params[:notes]
+#
+#   Contact.create(firstname: user_first_name,
+#     lastname: user_last_name,
+#     email: user_email,
+#     note: user_note)
+#
+#   # always redirect to ("/") on anything but a GET. On a GET use erb :file
+# end
+get '/' do
+  @contacts = Contact.all
+  @num_contacts = @contacts.count
+  erb :contacts
+end
+
+get '/contacts' do
+  redirect to '/'
+end
+
+get '/contacts/new' do
   erb :new
+end
+
+post '/contacts' do
+  Contact.create(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+  )
+  redirect to('/contacts')
 end
 
 
